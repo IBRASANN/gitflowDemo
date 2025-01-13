f7249f61e19e1c6e7ab4f5e674a82b1369f40175
Khalid-Kash <ibrasann12@gmail.com>
2025-01-13 23:15:52 +0300

    added calls to main with 10 attempts and a message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Game game = new Game(10);
+        System.out.println("starting now...");
+        game.play();
     }
 }
